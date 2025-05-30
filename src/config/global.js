2c87beeeb408acb2b7fb90a0f03ed1705b58a04a
Cielo-Angulo <cielodamarisangulo@gmail.com>
2025-05-29 20:11:00 -0500

    se hacen ajustes finales al CF

--- src/config/global.js
+++ src/config/global.js
@@ -191,7 +191,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/52450479_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
